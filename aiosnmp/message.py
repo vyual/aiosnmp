cb4f1bee07b65dff39916436af76fc0cacc92540
vyual <i@alisoff.ru>
2024-10-10 17:49:28 +0300

    undo dot deletion

--- aiosnmp/message.py
+++ aiosnmp/message.py
@@ -17,8 +17,8 @@ import ipaddress
 import random
 from typing import List, Optional, Union
 
-from asn1 import Class, Number
-from asn1_rust import Decoder, Encoder
+from .asn1 import Class, Number
+from .asn1_rust import Decoder, Encoder
 
 
 class SnmpVersion(enum.IntEnum):
